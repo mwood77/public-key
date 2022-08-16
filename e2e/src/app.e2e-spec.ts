ca22e07494a556852c1cb76ddbceb3f43544e463
mwood77 <43637076+mwood77@users.noreply.github.com>
2022-08-16 18:03:19 +0200

    prep release 2.2.0

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
 	it('should display welcome message', async () => {
 		await page.navigateTo();
-		expect(await page.getTitleText()).toEqual('public-key-site app is running!');
+		expect(await page.getTitleText()).toEqual('public-key app is running!');
 	});
 
 	afterEach(async () => {
